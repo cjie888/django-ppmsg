d7dbf1e7098d4b2c1041bfba1707da5d796d033b
cjie888 <cjie888@163.com>
2013-10-29 17:15:33 +0800

    add compose tag

--- ppmsg/templatetags/ppmsg.py
+++ ppmsg/templatetags/ppmsg.py
@@ -4,7 +4,28 @@ from django.template import Context
 
 register = template.Library()
 
-
 @register.filter(name='get_item')
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.tag("compose_msg")
+def do_compose_msg(parser, token):
+    try:
+        tag_name, next = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires two argument" % tag_name)
+    return ComposeMessageNode(next)
+
+
+class ComposeMessageNode(template.Node):
+    
+    def __init__(self, next):
+        self._next = template.Variable(next)
+        
+    def render(self, context):
+        t = template.loader.get_template("tags/compose.html")
+        next = self._next.resolve(context)
+        new_context = Context({'next': next}, autoescape=context.autoescape)
+        
+        return t.render(new_context)

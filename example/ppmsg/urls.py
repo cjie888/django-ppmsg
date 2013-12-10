abddcb7e3ccbd28733578795813f66f77cf198e5
cjie888 <cjie888@163.com>
2013-12-10 14:27:58 +0800

    例子同步添加发送框

--- example/ppmsg/urls.py
+++ example/ppmsg/urls.py
@@ -6,6 +6,7 @@ import settings
 urlpatterns = patterns('',
     url(r'compose/$', compose, name='messages_compose'),
     url(r'compose/(?P<recipient>[\w.@+-]+)/$', compose, name='messages_compose_to'),
+    url(r'send/(?P<recipient>[\w.@+-]+)/$', send_msg, name='messages_send_to'),
     url(r'search/$', view, name='messages_search'),
     url(r'view/$', view, name='messages_view'),
     url(r'detail/(?P<username>[\w.@+-]+)/$', view_detail, name='messages_detail'),
